d6f19cdedd896adb28fdc7edc955134c2bcf836d
Athikhom <athikhom.ra.57@ubu.ac.th>
2019-10-26 17:30:34 +0700

    upload file

---
+++ src/environments/firebase.config.ts
@@ -0,0 +1,11 @@
+export const  firebaseConfig = {
+
+  apiKey: "AIzaSyACds4BIsjkqGA9zCz89Etq-Wjz4WNfnCA",
+  authDomain: "project-card2019.firebaseapp.com",
+  databaseURL: "https://project-card2019.firebaseio.com",
+  projectId: "project-card2019",
+  storageBucket: "project-card2019.appspot.com",
+  messagingSenderId: "713254521003",
+  appId: "1:713254521003:web:75f139c2695e3cc41e8fee"
+   
+  };
